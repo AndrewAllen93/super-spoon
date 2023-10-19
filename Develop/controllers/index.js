990c4b1b2e58bb7ca03a0626ac91d757135a5605
tessaandrews <142922842+tessaandrews@users.noreply.github.com>
2023-10-19 10:39:39 -0500

    resetting

--- Develop/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const apiRoutes = require('./api');
-const userRoutes = require('./api/user-routes.js');
-
-router.use('/', userRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
