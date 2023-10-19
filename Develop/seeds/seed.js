0ca7470dd4b8638c7ffd7e0b64ac832e8c657f1a
Shocker <zacsmcdowell@gmail.com>
2023-10-18 23:30:20 -0400

    modified routes, seeds, homepage.handlebars

--- Develop/seeds/seed.js
+++ Develop/seeds/seed.js
@@ -1,15 +1,21 @@
-const sequelize = require('../config/connection');
-const { User } = require('../models');
-
+const sequelize = require('../Develop/config/connection'); // Import your Sequelize configuration
 const userData = require('./userData.json');
+const forumTopicsData = require('./forumTopics.json');
+const forumPostsData = require('./forumPosts.json');
+const User = require('../Develop/models/User');
+const ForumTopic = require('../Develop/models/ForumTopic');
+const ForumPost = require('../Develop/models/ForumPost');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
     individualHooks: true,
-    returning: true,
   });
+
+  await ForumTopic.bulkCreate(forumTopicsData);
+
+  await ForumPost.bulkCreate(forumPostsData);
 
   process.exit(0);
 };
