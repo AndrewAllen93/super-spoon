9d4c08c7f8e5bd4df7b5c26f61d28dd6c158d755
tessaandrews <142922842+tessaandrews@users.noreply.github.com>
2023-10-20 09:57:08 -0500

    updated seeds file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,10 @@
 const sequelize = require('../config/connection'); // Import your Sequelize configuration
-const userData = require('./userData.json');
+const userData = require('./userData');
 const CommentsData = require('./Comments');
 const blogPostData = require('./blogData');
 const User = require('../models/User');
-const { blogPost } = require('../models');
-const { Comments } = require('../models');
+const { blogPost } = require('../models/blogPost');
+const { Comments } = require('../models/Comments');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
